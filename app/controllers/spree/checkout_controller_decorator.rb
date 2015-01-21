0905e3f7159377ead3e27ca6b26de268d6cc9cd0
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-01-21 02:50:05 -0300

    hacer de cuenta que los pagos vienen completos

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -13,24 +13,10 @@ module Spree
 
         return unless @payment_method.try(:kind_of?, Spree::BillingIntegration::Dineromail)
 
-        # Para todos los pagos que se hagan con dineromail, verificar
-        # con el IPN que se haya completado el proceso con ellos.
         @order.payments.where(payment_method: @payment_method).each do |payment|
-
           payment.started_processing!
-
-          ipn = DineroMailIpn::Client.new(account: @payment_method.preferences[:merchant],
-                                          password: @payment_method.preferences[:password])
-          t = ipn.consulta_transacciones([@order.number]).reports.first
-
-          if t.valid? && t.amount == @order.total.to_money.cents
-            payment.pend!       if t.transaction_pending?
-            payment.complete!   if t.transaction_completed?
-            payment.invalidate! if t.transaction_cancelled?
-          else
-            # El pago no sirve si no se realizó en DineroMail
-            payment.invalidate!
-          end
+          # Hacer de cuenta que todos los pagos vienen completos
+          payment.complete!
         end
       end
   end
